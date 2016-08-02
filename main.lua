47e0a8c6c509398f28b7b0a7bacf11a3170cdeea
Rob Miracle <rob@coronalabs.com>
2016-08-02 17:53:10 -0400

    Tweaked audio volume slightly

--- main.lua
+++ main.lua
@@ -10,7 +10,7 @@ math.randomseed( os.time() )
 -- Reserve channel 1 for background music
 audio.reserveChannels( 1 )
 -- Reduce the overall volume of the channel
-audio.setVolume( 0.4, { channel=1 } )
+audio.setVolume( 0.5, { channel=1 } )
 
 -- Go to the menu screen
 composer.gotoScene( "menu" )
